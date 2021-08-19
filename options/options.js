a5e9fa69c955bf534bc3a9511c6a3d2c9cb690c8
Crowdbotics <team@crowdbotics.com>
2021-08-19 19:55:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shiny_sunset_29954Identifier",
+  url: "https://shiny_sunset_29954Identifier.botics.co",
+  api: "https://shiny_sunset_29954Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
